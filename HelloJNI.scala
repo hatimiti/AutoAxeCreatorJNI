da85860b207bf5749048cc753b5689794f701c6a
hatimiti <hatti33@gmail.com>
2016-06-13 08:10:22 +0900

    Fixes a scala file.

--- HelloJNI.scala
+++ HelloJNI.scala
@@ -3,8 +3,12 @@ object HelloJNI {
   def main(args: Array[String]): Unit = {
     System.loadLibrary("hello");
     
-    hello;
+    val h = new HelloJNI();
+    h.hello
   }
 
+}
+
+class HelloJNI {
   @native def hello(): Unit
 }
